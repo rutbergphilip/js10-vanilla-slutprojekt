0eb9fdf847c49b9d1508742bf27cae021cb69b1a
Philip Rutberg <philiprutberg00@gmail.com>
2020-11-08 00:05:58 +0100

    Added randomized beer upon page load

--- js/main.js
+++ js/main.js
@@ -1,8 +1,31 @@
+/* ----- Fetching the API ----- */
+
+async function getBeerData() {
+    // Declaring and getting the API
+    const request = await fetch('https://api.punkapi.com/v2/beers')
+    const response = await request.json()
+    return response
+};
+
+//console.log(getBeerData())
+
+/* ----- Random Beer On Load ----- */
+
+getBeerData().then(beers => {
+    // Randomizing beers and putting it inside a variable
+    const randomBeer = beers[Math.floor(Math.random() * beers.length)]
+
+    // Display the fetched random beer info
+    document.querySelector(".image-bord").src = randomBeer.image_url
+    document.querySelector("h2").innerText = randomBeer.name
+
+})
+
+/* ----- Modal ----- */
+
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
-
-/* ----- Modal ----- */
 
 openModalButtons.forEach(button => {
     button.addEventListener('click', () => {
