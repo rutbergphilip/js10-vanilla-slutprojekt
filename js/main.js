3328c2dd700a22e6e08393aa5fdc0565d95340a7
dabbe01 <dabbe01@bredband.net>
2020-11-12 17:02:53 +0100

    Summary

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 /*       Fetching the API       */
 /*------------------------------*/
 
-async function getBeerData() {
+async function getRandomBeerData() {
     // Declaring and getting the API
     const request = await fetch('https://api.punkapi.com/v2/beers/random')
     const response = await request.json()
@@ -14,7 +14,7 @@ async function getBeerData() {
 /*---------------------------------*/
 
 function fetchBeerInfo() {
-    getBeerData().then(beers => {
+    getRandomBeerData().then(beers => {
         // Randomizing beers and putting it inside a variable
         const randomBeer = beers[0]
 
@@ -26,7 +26,7 @@ function fetchBeerInfo() {
         document.querySelector(".title").innerText = randomBeer.name
 
         document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<h4>Description</h4>"
-        document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = randomBeer.description
+        document.querySelector(".modal-body").appendChild(document.createElement("p-description")).innerText = randomBeer.description
 
         document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Alcohol by volume</h4>"
         document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = randomBeer.abv
@@ -46,7 +46,7 @@ function fetchBeerInfo() {
         document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Brewers tips</h4>"
         document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = randomBeer.brewers_tips
 
-        // document.querySelector(".modal-image").src = randomBeer.image_url
+        document.querySelector(".modal-image").src = randomBeer.image_url
     })
 }
 fetchBeerInfo()
@@ -70,16 +70,19 @@ randomizeBeer()
 /*       Navbar       */
 /*--------------------*/
 
+// Style for open navbar
 function openNav() {
     document.querySelector(".sidenav").style.width = "250px";
     document.querySelector("nav").style.marginLeft = "0";
 }
 
+// Style for closed navbar
 function closeNav() {
     document.querySelector(".sidenav").style.width = "0";
     document.querySelector("nav").style.marginLeft = "0";
 }
 
+// Adding listeners to the nav buttons
 document.querySelector("nav").addEventListener('click', openNav)
 document.querySelector(".nav1").addEventListener('click', openNav)
 
@@ -89,20 +92,25 @@ document.querySelector(".closebtn").addEventListener('click', closeNav)
 /*     Page navigation     */
 /*-------------------------*/
 
+// Declaring the wrappers and pages
 const wrapHome = document.querySelector(".wrap-home")
 const wrapSearch = document.querySelector(".wrap-search")
 const beerWiki = document.querySelector(".beer-wiki")
 const searchPage = document.querySelector(".search-page")
 
+// Setting which page should be default on load
 wrapHome.classList.add("active")
 wrapSearch.classList.remove("active")
 
+// Clicking on the Search Page hides the Beer Wiki section
 searchPage.addEventListener('click', () => {
     wrapHome.classList.add("active")
     wrapSearch.classList.remove("active")
 
     closeNav()
 })
+
+// Clicking on the Beer Wiki hides the Search Page section
 beerWiki.addEventListener('click', () => {
     wrapHome.classList.remove("active")
     wrapSearch.classList.add("active")
@@ -122,40 +130,72 @@ async function getBeerDataOnSearch(userInput) {
     return res
 }
 
+function fetchClickedBeerInfo() {
+    getBeerDataOnSearch().then(beer => {
+        // Randomizing beers and putting it inside a variable
+        const clickedBeer = beer.target.dataset
+
+        // Display the fetched random beer on page load
+        document.querySelector(".image-bord").src = randomBeer.image_url
+        document.querySelector("h2").innerText = randomBeer.name
+
+        // Display the fetched random beer info on modal
+        document.querySelector(".title").innerText = randomBeer.name
+
+        document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<h4>Description</h4>"
+        document.querySelector(".modal-body").appendChild(document.createElement("p-description")).innerText = randomBeer.description
+
+        document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Alcohol by volume</h4>"
+        document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = randomBeer.abv
+
+        document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Volume</h4>"
+        document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = `${randomBeer.volume.value} ${randomBeer.volume.unit}`
+
+        document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Ingredients</h4>"
+        document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = getIngredients(randomBeer.ingredients.malt)
+
+        document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Hops</h4>"
+        document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = getIngredients(randomBeer.ingredients.hops)
+
+        document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Food pairing</h4>"
+        document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = randomBeer.food_pairing
+
+        document.querySelector(".modal-body").appendChild(document.createElement("div")).innerHTML = "<br><h4>Brewers tips</h4>"
+        document.querySelector(".modal-body").appendChild(document.createElement("p")).innerText = randomBeer.brewers_tips
+
+        document.querySelector(".modal-image").src = randomBeer.image_url
+    })
+}
+fetchClickedBeerInfo()
+
 // Declaring the search btn
 const searchBtn = document.querySelector(".search-beer")
+const inputBox = document.querySelector("input")
 let searchBeerResult = []
 
 async function searchResult() {
-    // Declaring the user input box
     clearNav()
+        // Declaring the user input value
     const userInput = document.querySelector("input").value.toLowerCase()
     console.log(userInput)
 
+    // Wait for the results on user input
     searchBeerResult = await getBeerDataOnSearch(userInput)
     console.log(searchBeerResult)
 
+    // Calculating the number of pages necessary based on the result
     const numberOfPages = Math.ceil(searchBeerResult.length / 10)
     creatNav(numberOfPages)
-        /*const saveResult = []
 
-
-        for (let i = 0; i < beer.length; i++) {
-            saveResult.push(beer[i])
-        }*/
-
-    //console.log(saveResult)
+    // Sending in 1 because we want to see the first batch of beers on the result array
+    // Putting a 2 would show us everything on page 2
     renderData(1)
 }
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-
-
 function renderData(pageNumber) {
+    // Declaring the <ul>
     const nameOfUl = document.querySelector(".search-result")
     nameOfUl.innerHTML = ""
-    num = 1
-    pn = num * 10
 
     const startIndex = (pageNumber - 1) * 10
     let endIndex
@@ -166,70 +206,63 @@ function renderData(pageNumber) {
         endIndex = pageNumber * 10
     }
 
+    // Loop through and add the beers to the list
     for (let i = startIndex; i < endIndex; i++) {
         addItemToUl(searchBeerResult[i], nameOfUl)
     }
-
 }
 
+
+// Function to create the navbar for search page result navigation
 function creatNav(pageNumber) {
+    // For loop will help us create a button for each page necessary
     for (let n = 0; n < pageNumber; n++) {
-        const nav = document.querySelector(".pagination")
-        const link = document.createElement("button")
+        const nav = document.querySelector(".page-buttons")
+        const btn = document.createElement("button")
 
-        link.href = "#"
-        link.innerText = n + 1
+        // Setting the button name to n + 1, showing the page number on the buttons starting from 1
+        btn.href = "#"
+        btn.innerText = n + 1
 
-        link.addEventListener('click', changePages)
+        btn.addEventListener('click', changePages)
 
-        nav.append(link)
+        nav.append(btn)
     }
 }
 
-function clearNav() {
-    const nav = document.querySelector(".pagination")
-    nav.innerHTML = ''
-}
-
-function changePages(e) {
-    const num = e.target.innerText
-    console.log(num)
-        //console.log(pageNumber)
-
-    renderData(num)
-}
-
-function clearBeer() {
-    const nameOfUl = document.querySelector(".search-result")
-    nameOfUl.innerHTML = ""
-}
-
-function addItemToUl(item, nameOfUl) {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = item.name;
-    nameOfUl.append(listItem);
-}
-
+// Listener to the search button and the inputbox, when clicked, the previous search results should be wiped and the new result should be showcased
 searchBtn.addEventListener('click', () => {
     clearBeer()
     searchResult()
+})
+
+inputBox.addEventListener('keyup', (event) => {
+    if (event.keyCode === 13) {
+        clearBeer()
+        searchResult()
+    }
 })
 
 /*--------------------*/
 /*        Modal       */
 /*--------------------*/
 
+// Declaring the modal targets for open and close, as well as the overlay
 const openModalButtons = document.querySelectorAll('[data-modal-target]')
 const closeModalButtons = document.querySelectorAll('[data-close-button]')
 const overlay = document.getElementById('overlay')
 
+// Break out the modal buttons to target the links inside search page easier
+const modalCallback = event => {
+    const modal = document.querySelector(event.target.dataset.modalTarget)
+    openModal(modal)
+}
+
 openModalButtons.forEach(button => {
-    button.addEventListener('click', () => {
-        const modal = document.querySelector(button.dataset.modalTarget)
-        openModal(modal)
-    })
+    button.addEventListener('click', modalCallback)
 })
 
+// Targeting all 'active' overlays for closing on click outside the modal body
 overlay.addEventListener('click', () => {
     const modals = document.querySelectorAll('.modal.active')
     modals.forEach(modal => {
@@ -237,6 +270,7 @@ overlay.addEventListener('click', () => {
     })
 })
 
+// Add listener to the modal, looking for the closest modal with class modal
 closeModalButtons.forEach(button => {
     button.addEventListener('click', () => {
         const modal = button.closest('.modal')
@@ -244,12 +278,14 @@ closeModalButtons.forEach(button => {
     })
 })
 
+// Open Modal function, adding active to the modal body and overlay
 function openModal(modal) {
     if (modal == null) return
     modal.classList.add('active')
     overlay.classList.add('active')
 }
 
+// Close Modal function, adding active to the modal body and overlay
 function closeModal(modal) {
     if (modal == null) return
     modal.classList.remove('active')
@@ -274,4 +310,38 @@ function getIngredients(ingredients) {
 // Prevent form from refreshing the page
 function preventDefault(event) {
     event.preventDefault();
+}
+
+function clearNav() {
+    // Clearing the navbar 
+    const nav = document.querySelector(".page-buttons")
+    nav.innerHTML = ""
+}
+
+function changePages(e) {
+    // Changing pages based on the clicked page button
+    const num = e.target.innerText
+    console.log(num)
+
+    renderData(num)
+}
+
+function clearBeer() {
+    // Clearing the beer results
+    const nameOfUl = document.querySelector(".search-result")
+    nameOfUl.innerHTML = ""
+}
+
+function addItemToUl(item, nameOfUl) {
+    // Creating the <li> and <a> format to add to the <ul>
+    const listItem = document.createElement('li')
+    const listAnchor = document.createElement('a')
+
+    listItem.append(listAnchor)
+    listAnchor.href = '#'
+    listAnchor.setAttribute('data-modal-target', '#modal')
+    listAnchor.addEventListener('click', modalCallback)
+    listAnchor.innerHTML = item.name
+
+    nameOfUl.append(listItem)
 }
